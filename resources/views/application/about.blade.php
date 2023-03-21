70230c5a9c697b030f18ef305e7a778379bfab37
Stanley2231163 <ernastanley@gmail.com>
2023-03-21 22:46:12 +0700

    stanleycv

---
+++ resources/views/application/about.blade.php
@@ -0,0 +1,60 @@
+@extends('layout.app')
+
+@section('title', 'About')
+
+@section('about')
+    <section id="about" class="about">
+        <div class="container" data-aos="fade-up">
+
+            <div class="section-title">
+                <h2>About</h2>
+                <p>Hello , My name is Stanley , i'm college student from Batam International University , i am majoring in
+                    Information System </p>
+                <blockquote>"Never Surrender"</blockquote>
+            </div>
+
+            <div class="row">
+                <div class="col-lg-4">
+                    <img src="assets/img/stanley.jpg" class="img-fluid" alt="">
+                </div>
+                <div class="col-lg-8 pt-4 pt-lg-0 content">
+                    <h3>College Student &amp; Gamers</h3>
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <ul>
+                                <li><i class="bi bi-chevron-right"></i> <strong>Birthday:</strong> <span>30 Februari
+                                        1999</span>
+                                </li>
+                                <li><i class="bi bi-chevron-right"></i> <strong>Phone:</strong> <span>+621 43356 7890</span>
+                                </li>
+                                <li><i class="bi bi-chevron-right"></i> <strong>City:</strong> <span>Batam, Indonesia</span>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-lg-6">
+                            <ul>
+                                <li><i class="bi bi-chevron-right"></i> <strong>Age:</strong> <span>24</span></li>
+                                <li><i class="bi bi-chevron-right"></i> <strong>Degree:</strong> <span>ML Legend</span></li>
+                                <li><i class="bi bi-chevron-right"></i> <strong>Email:</strong>
+                                    <span>stanleycarryanimasi@gmail.com</span>
+                                </li>
+                                <li><i class="bi bi-chevron-right"></i> <strong>Freelance:</strong> <span>Unavailable</span>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <p>
+                        Je suis une espèce humaine terrestre, pas un humain martien, ma mission est de survivre sur terre et
+                        de porter l'animation dans mon groupe Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem
+                        ipsum dolor sit amet consectetur adipisicing elit. Veniam, facilis. Lorem ipsum dolor sit amet
+                        consectetur adipisicing elit. Rem non voluptas, esse nihil veniam quis deserunt, harum quasi amet
+                        quae facere vero optio nisi culpa veritatis, vitae aliquid? Adipisci enim iusto tempore quae earum
+                        esse natus, nostrum, magnam ducimus consectetur maiores molestiae voluptates vero facere eum ipsum
+                        fugit atque nulla!
+                    </p>
+                </div>
+            </div>
+
+        </div>
+    </section><!-- End About Section -->
+@endsection
---
+++ resources/views/application/home.blade.php
@@ -0,0 +1,16 @@
+@extends('layout.app')
+
+@section('title', 'Home')
+
+@section('home')
+    <section id="hero" class="d-flex flex-column justify-content-center">
+        <div class="container" data-aos="zoom-in" data-aos-delay="100">
+            <h1>Stanley</h1>
+            <p>I'm <span class="typed" data-typed-items="College Student, Animator, Designer, Gamers"></span></p>
+            <div class="social-links">
+                <a href="https://instagram.com/stanleyrajinbelajar" class="instagram"><i class="bx bxl-instagram"></i></a>
+                <a href="https://github.com/stanley2231163" class="github"><i class="bx bxl-github"></i></a>
+            </div>
+        </div>
+    </section>
+@endsection
